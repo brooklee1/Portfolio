44d5f7c029f1bbb656cd6e7e015a6142bc759077
brooklee1 <brooklee.wilson@gmail.com>
2019-04-18 20:54:43 -0600

    added syntax

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,43 +2,38 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
+import Prism from "prismjs"
+import "../global/css/prism.css"
 
 const AboutWrapper = styled.div`
   display: grid;
   grid-template-columns: 70% 30%;
   position: relative;
-  height: 100vh;
+  /* height: 100vh; */
+  height: auto;
   scroll-snap-align: start;
 
   @media (max-width: 860px) {
-     height: auto;
+    height: auto;
     grid-template-columns: 1fr;
   }
 `
 
 const Title = styled.h2`
-  color: #4700ba;
-  font-family: "Source Code Pro", monospace;
-  font-size: 30px;
   margin: 10px 50px;
   padding-top: 50px;
 
   @media (max-width: 700px) {
-    font-size: 25px;
     margin: 10px 15px;
   }
 `
 
 const Title2 = styled.h2`
-  color:rgba(255,255,255,.7);
-  font-family: "Source Code Pro", monospace;
-  font-size: 30px;
   margin: 10px 50px;
   padding-top: 50px;
   text-align: center;
 
   @media (max-width: 700px) {
-    font-size: 25px;
     margin: 10px 15px;
   }
 `
@@ -61,17 +56,17 @@ const ImagePortfolio = styled.img`
   transform: translateY(-50%);
   right: 20%;
 
-   @media (max-width: 2000px) {
+  @media (max-width: 2000px) {
     right: 20%;
   }
 
- @media (max-width: 1730px) {
+  @media (max-width: 1730px) {
     max-width: 300px;
     right: 15%;
   }
 
-  @media (max-width: 860px){
-    display:none;
+  @media (max-width: 860px) {
+    display: none;
   }
 `
 
@@ -94,7 +89,7 @@ const SocialFollow = styled.div`
   grid-template-columns: repeat(4, auto);
   justify-items: center;
 
-  @media (max-width: 445px){
+  @media (max-width: 445px) {
     grid-template-columns: repeat(2, auto);
   }
 `
@@ -118,17 +113,16 @@ const SocialImg1 = styled.div`
 
   :hover {
     transform: scale(1.2, 1.2);
- 
   }
 
   @media (max-width: 1730px) {
-  top: 35%;
-  /* transform: translateY(-35%); */
-  right: 17%;
+    top: 29%;
+    /* transform: translateY(-35%); */
+    right: 17%;
   }
 
-  @media (max-width: 860px){
-     position:static;
+  @media (max-width: 860px) {
+    position: static;
   }
 `
 const SocialImg2 = styled.img`
@@ -141,17 +135,16 @@ const SocialImg2 = styled.img`
 
   :hover {
     transform: scale(1.2, 1.2);
- 
   }
 
   @media (max-width: 1730px) {
-  top: 43%;
-  /* transform: translateY(-43%); */
-  right: 11%;
+    top: 41%;
+    /* transform: translateY(-43%); */
+    right: 11%;
   }
 
-  @media (max-width: 860px){
-    position:static;
+  @media (max-width: 860px) {
+    position: static;
   }
 `
 
@@ -165,21 +158,21 @@ const SocialImg3 = styled.img`
 
   :hover {
     transform: scale(1.2, 1.2);
- 
   }
 
   @media (max-width: 1730px) {
-  top: 55%;
-  /* transform: translateY(-55%); */
-  right: 11%;
+    top: 55%;
+    /* transform: translateY(-55%); */
+    right: 11%;
   }
 
-  @media (max-width: 860px){
-     position:static;
+  @media (max-width: 860px) {
+    position: static;
   }
 `
 
 const SocialImg4 = styled.img`
+  z-index: 42;
   position: absolute;
   top: 65%;
   /* transform: translateY(-65%); */
@@ -189,55 +182,94 @@ const SocialImg4 = styled.img`
 
   :hover {
     transform: scale(1.2, 1.2);
- 
   }
 
   @media (max-width: 1730px) {
-  top: 65%;
-  /* transform: translateY(-65%); */
-  right: 17%;
+    top: 65%;
+    /* transform: translateY(-65%); */
+    right: 17%;
   }
 
-  @media (max-width: 860px){
-    position:static;
+  @media (max-width: 860px) {
+    position: static;
   }
 `
 
-
+Prism.highlightAll()
 
 const AboutSection = ({ siteTitle }) => (
   <AboutWrapper id="About">
     <AboutGroup>
-      <Title>{"<About/>"}</Title>
-      <IFrame
-        src="https://carbon.now.sh/embed/?bg=rgba(1%2C2%2C15%2C1)&t=night-owl&wt=bw&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fm=Hack&fs=14px&lh=133%25&si=false&es=4x&wm=false&code=Class%2520Brooklee_Watters%2520extends%2520Component%2520%257B%250A%2520%250A%25C2%25B7%25C2%25B7%252F%252F%2520Love%2520a%2520good%2520challenge%2520and%2520subscribe%2520to%2520the%2520KISS%2520method%250A%25C2%25B7%25C2%25B7%252F%252F%2520Always%2520expanding%2520my%2520skill%2520set.%250A%250A%25C2%25B7%25C2%25B7constructor()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7this.name%2520%253D%2520%25E2%2580%2598Brooklee%2520Watters%25E2%2580%2599%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7this.email%2520%253D%2520%25E2%2580%2598brooklee.wilson%2540gmail.com%25E2%2580%2599%250A%25C2%25B7%25C2%25B7%257D%250A%250A%25C2%25B7%25C2%25B7workExperience()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7return%2520%255B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2018-now'%2520%253A%2520%25E2%2580%2598Web%2520Dev%252FDesigner%2520at%2520Ultradent%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2018-now%25E2%2580%2599%253A%2520%25E2%2580%2598Teacher%2520Aide%2520(web%2520course)%2520at%2520Trillogy%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2017-2018%25E2%2580%2599%253A%2520%25E2%2580%2598Web%2520Dev%252FAnalyst%2520at%2520CrossRoads%2520Tech%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2012-2017%25E2%2580%2599%253A%2520'Graphic%2520Designer%2520at%2520DigiDocs%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%255D%250A%25C2%25B7%25C2%25B7%257D%250A%250A%25C2%25B7%25C2%25B7education()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7return%2520%255B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2017-2018'%253A%2520'Google%2520Developer%2520Challenge%2520Scholarship%2520-%2520Mobile%2520Web%2520Specialist'%2520%257D%252C%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2012-2014'%253A%2520'Wy%25C5%25BCsza%2520Szko%25C5%2582a%2520Biznesu%2520w%2520D%25C4%2585browie%2520G%25C3%25B3rniczej%2520-%2520mgr%2520IT'%2520%257D%252C%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2008-2012'%253A%2520'Wy%25C5%25BCsza%2520Szko%25C5%2582a%2520Technologii%2520Informatycznych%2520w%2520Katowicach%2520-%2520in%25C5%25BC.%2520IT'%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%255D%250A%25C2%25B7%25C2%25B7%257D%250A%250A%25C2%25B7%25C2%25B7skills()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7return%2520%255B%2520'HTML%252FCSS%252FJS'%252C%2520%25E2%2580%2598React.js'%252C%2520'jQuery'%252C%2520'Node.js'%252C%2520'Bootstrap%252FTailwind%252FMaterial'%252C%2520'SASS'%252C%2520'npm%252Fyarn'%252C%2520'GIT'%252C%2520'Firebase%252C%2520'MySQL'%252C%2520'Photoshop'%252C%2520'Illustrator'%252C%2520'Sketch'%252C%2520'UX%252FUI'%252C%255D%250A%25C2%25B7%25C2%25B7%257D%250A%257D"
+      <Title>
+        <span className="purpleH2">{"<"}</span>About
+        <span className="purpleH2">{"/>"}</span>
+      </Title>
+      <pre className="language-javascript">
+        <code className="language-javascript">{`Class Brooklee_Watters extends Component {
+ 
+ ··// Love a good challenge and subscribe to the KISS method
+ ··// Always expanding my skill set.
+ 
+ ··constructor() {
+ ····this.name = ‘Brooklee Watters’
+ ····this.email = ‘brooklee.wilson@gmail.com’
+ ··}
+ 
+ ··workExperience() {
+ ····return [
+ ······{ '2018-now' : ‘Web Dev/Designer at Ultradent’ }
+ ······{ '2018-2019’: ‘Teacher Aide (web course) at Trillogy’ }
+ ······{ '2017-2018’: ‘Web Dev/Analyst at CrossRoads Tech’ }
+ ······{ '2012-2017’: 'Graphic Designer at DigiDocs’ }
+ ····]
+ ··}
+ 
+ ··education() {
+ ····return [
+ ······{ '2017-2017’: ‘University of Utah Coding Bootcamp’ },
+ ······{ '2010-2012’: ‘Associates Graphic Design’ },
+ ····]
+ ··}
+ 
+ ··skills() {
+ ····return [ 'HTML/CSS/JS', ‘React.js', 'jQuery', 'Node.js', 
+ 'Bootstrap/Tailwind/Material', 'SASS', 'npm/yarn', 'GIT', 
+ 'Firebase', ‘MongoDB’, 'MySQL', 'Photoshop', 'Illustrator', 
+ 'Sketch', 'UX/UI',]
+ ··}
+ }`}</code>
+      </pre>
+      {/* <IFrame
+        src="https://carbon.now.sh/embed/?bg=rgba(1%2C2%2C15%2C1)&t=dracula&wt=bw&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fm=Hack&fs=14px&lh=133%25&si=false&es=4x&wm=false&code=Class%2520Brooklee_Watters%2520extends%2520Component%2520%257B%250A%2520%250A%25C2%25B7%25C2%25B7%252F%252F%2520Love%2520a%2520good%2520challenge%2520and%2520subscribe%2520to%2520the%2520KISS%2520method%250A%25C2%25B7%25C2%25B7%252F%252F%2520Always%2520expanding%2520my%2520skill%2520set.%250A%250A%25C2%25B7%25C2%25B7constructor()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7this.name%2520%253D%2520%25E2%2580%2598Brooklee%2520Watters%25E2%2580%2599%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7this.email%2520%253D%2520%25E2%2580%2598brooklee.wilson%2540gmail.com%25E2%2580%2599%250A%25C2%25B7%25C2%25B7%257D%250A%250A%25C2%25B7%25C2%25B7workExperience()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7return%2520%255B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2018-now'%2520%253A%2520%25E2%2580%2598Web%2520Dev%252FDesigner%2520at%2520Ultradent%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2018-2019%25E2%2580%2599%253A%2520%25E2%2580%2598Teacher%2520Aide%2520(web%2520course)%2520at%2520Trillogy%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2017-2018%25E2%2580%2599%253A%2520%25E2%2580%2598Web%2520Dev%252FAnalyst%2520at%2520CrossRoads%2520Tech%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2012-2017%25E2%2580%2599%253A%2520'Graphic%2520Designer%2520at%2520DigiDocs%25E2%2580%2599%2520%257D%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%255D%250A%25C2%25B7%25C2%25B7%257D%250A%250A%25C2%25B7%25C2%25B7education()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7return%2520%255B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2017-2017%25E2%2580%2599%253A%2520%25E2%2580%2598University%2520of%2520Utah%2520Coding%2520Bootcamp%25E2%2580%2599%2520%257D%252C%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%257B%2520'2010-2012%25E2%2580%2599%253A%2520%25E2%2580%2598Associates%2520Graphic%2520Design%25E2%2580%2599%2520%257D%252C%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7%255D%250A%25C2%25B7%25C2%25B7%257D%250A%250A%25C2%25B7%25C2%25B7skills()%2520%257B%250A%25C2%25B7%25C2%25B7%25C2%25B7%25C2%25B7return%2520%255B%2520'HTML%252FCSS%252FJS'%252C%2520%25E2%2580%2598React.js'%252C%2520'jQuery'%252C%2520'Node.js'%252C%2520'Bootstrap%252FTailwind%252FMaterial'%252C%2520'SASS'%252C%2520'npm%252Fyarn'%252C%2520'GIT'%252C%2520'Firebase%252C%2520%25E2%2580%2598MongoDB%25E2%2580%2599%252C%2520'MySQL'%252C%2520'Photoshop'%252C%2520'Illustrator'%252C%2520'Sketch'%252C%2520'UX%252FUI'%252C%255D%250A%25C2%25B7%25C2%25B7%257D%250A%257D"
         sandbox="allow-scripts allow-same-origin"
-      />
+      /> */}
       <About />
     </AboutGroup>
     <ImagePortfolio src={require("../images/Portfolio_Img.png")} />
     <SocialFollowWrapper>
-    <Title2>Stalk me</Title2>
-    <SocialFollow>
-      <SocialImg1>
-        <a href="https://codepen.io/brooklee/" target="_blank">
-          <img src={require("../images/CodePen_Logo.svg")} />
-        </a>
-      </SocialImg1>
+      <Title2>
+        <span className="purpleH2">{"<"}</span>Stalk me
+        <span className="purpleH2">{"/>"}</span>
+      </Title2>
+      <SocialFollow>
+        <SocialImg1>
+          <a href="https://codepen.io/brooklee/" target="_blank">
+            <img src={require("../images/CodePen_Logo.svg")} />
+          </a>
+        </SocialImg1>
 
         <a href="https://github.com/brooklee1" target="_blank">
-         <SocialImg2 src={require("../images/Git_Logo.png")} />
+          <SocialImg2 src={require("../images/Git_Logo.svg")} />
         </a>
 
         <a href="https://dribbble.com/brooklee" target="_blank">
-         <SocialImg3 src={require("../images/dribbble-ball-mark.svg")} />
+          <SocialImg3 src={require("../images/dribbble-ball-mark.svg")} />
         </a>
 
-        <a href="https://www.behance.net/brookleewilson" target="_bla</a>nk">
+        <a href="https://www.behance.net/brookleewilson" target="_blank">
           <SocialImg4 src={require("../images/Behance_Logo.svg")} />
         </a>
-
-    </SocialFollow>
+      </SocialFollow>
     </SocialFollowWrapper>
   </AboutWrapper>
 )
